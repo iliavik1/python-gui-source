5357e2c658db508901f33a52e9700acb10661267
iliavik1 <downloadermeneger@gmail.com>
2024-07-01 16:01:13 +0200

    new version
    1.1 v
    || add button and pos

--- tttt/main.py
+++ tttt/main.py
@@ -8,6 +8,10 @@ label = tk.Label(root, text="hi this is my first program on python", font=('Aria
 label.pack(padx=30, pady=30)
 label1 = tk.Label(root, text="this is made for github by iliavik1", font=('Arial', 30))
 label1.pack(padx=30, pady=30)
+button = tk.Button(root, text="button", font=('Arial', 15))
+button.pack(padx=30, pady=30)
+button.place(x=400, y=200, height=100, width=200)
+
 root.mainloop()
 
 
