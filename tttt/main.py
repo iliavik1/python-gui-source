a1e37903fe48373ff53a238dcdb69ae65c2adfd1
iliavik1 <downloadermeneger@gmail.com>
2024-07-01 15:27:02 +0200

    new version 1.0
    || add new text

--- tttt/main.py
+++ tttt/main.py
@@ -6,6 +6,8 @@ root.geometry("1000x500")
 root.title("hi")
 label = tk.Label(root, text="hi this is my first program on python", font=('Arial', 18))
 label.pack(padx=30, pady=30)
+label1 = tk.Label(root, text="this is made for github by iliavik1", font=('Arial', 30))
+label1.pack(padx=30, pady=30)
 root.mainloop()
 
 
